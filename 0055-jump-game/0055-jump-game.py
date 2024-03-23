f83c4e4465c4fe00e3d8a0485546ddaef259e201
Shashank Reddy <76064165+Shrux2k@users.noreply.github.com>
2024-03-23 19:32:17 +0530

    Time: 346 ms (80.25%), Space: 17.9 MB (57.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,22 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        
-        
-        #make a pointer and jump based on next index
-        #if it lands on the last index then return true
-        #if it lands way after the last index then return false
-        #wherever it jumps holds a value 0 then return false 
-        
-        
-      gas = 0
-      for n in nums:
-        if gas < 0:
-            return False
-        elif n > gas:
-            gas = n
-        gas -= 1
-            
-      return True
-            
-        
-        
+        goal = len(nums)-1
+        for i in range(len(nums) - 1,-1,-1):
+            if i + nums[i] >= goal:
+                goal = i
+                
+        return True if goal == 0 else False
